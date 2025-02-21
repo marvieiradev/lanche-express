25a3c251f9d865e4caf4c1f794c59c4bd9a08ea7
marvieiradev <marcosvieira482@gmail.com>
2025-02-20 22:10:16 -0300

    feat: add scroll to product details page

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -1,38 +1,43 @@
-"use client"
-import { Button } from "@/components/ui/button";
-import { Product } from "@prisma/client";
-import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+'use client'
+import { Button } from '@/components/ui/button'
+import { Product } from '@prisma/client'
+import { ChevronLeftIcon, ScrollTextIcon } from 'lucide-react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 interface ProductHeaderProps {
-    product: Pick<Product, 'name' | 'imageUrl'>
+  product: Pick<Product, 'name' | 'imageUrl'>
 }
 
 const ProductHeader = ({ product }: ProductHeaderProps) => {
-    const router = useRouter();
-    const handleBackClick = () => router.back();
+  const router = useRouter()
+  const handleBackClick = () => router.back()
 
-    return (
-        <div className="relative h-[300px] w-full">
-            <Button
-                variant="secondary"
-                size="icon"
-                className="absolute left-4 top-4 z-50 rounded-full"
-                onClick={handleBackClick}
-            >
-                <ChevronLeftIcon />
-            </Button>
-            <Image src={product.imageUrl} alt={product.name} fill className="object-contain" />
-            <Button
-                variant="secondary"
-                size="icon"
-                className="absolute right-4 top-4 z-50 rounded-full"
-            >
-                <ScrollTextIcon />
-            </Button>
-        </div>
-    );
+  return (
+    <div className='relative min-h-[300px] w-full'>
+      <Button
+        variant='secondary'
+        size='icon'
+        className='absolute left-4 top-4 z-50 rounded-full'
+        onClick={handleBackClick}
+      >
+        <ChevronLeftIcon />
+      </Button>
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className='object-contain'
+      />
+      <Button
+        variant='secondary'
+        size='icon'
+        className='absolute right-4 top-4 z-50 rounded-full'
+      >
+        <ScrollTextIcon />
+      </Button>
+    </div>
+  )
 }
 
-export default ProductHeader;
+export default ProductHeader
