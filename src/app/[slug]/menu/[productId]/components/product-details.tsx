8171cb4dc8a420e3fc2cdc77086c02a5af50acfb
marvieiradev <marcosvieira482@gmail.com>
2025-02-20 22:37:57 -0300

    feat: add initial cart context and card sheet components

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -5,7 +5,9 @@ import { formatCurrency } from '@/helpers/format-currency'
 import { Prisma, Product, Restaurant } from '@prisma/client'
 import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from 'lucide-react'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { CartContext } from '../../context/cart'
+import CartSheet from '../../components/cart-sheet'
 
 interface ProductDetailsProps {
   product: Prisma.ProductGetPayload<{
@@ -20,6 +22,7 @@ interface ProductDetailsProps {
   }>
 }
 const ProductDetails = ({ product }: ProductDetailsProps) => {
+  const { isOpen, toggleCart } = useContext(CartContext)
   const [quantity, setQuantity] = useState<number>(1)
   const handleDecrease = () => {
     setQuantity((prev) => {
@@ -34,67 +37,76 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
     setQuantity((prev) => prev + 1)
   }
 
-  return (
-    <div className='flex flex-col relative z-50 mt-[-1.5rem] rounded-t-3xl p-5 flex-auto overflow-hidden'>
-      <div className='flex-auto overflow-hidden'>
-        <div className='flex items-center gap-1.5'>
-          <Image
-            src={product.restaurant.avatarImageUrl}
-            alt={product.restaurant.name}
-            width={16}
-            height={16}
-            className='rounded-full'
-          />
-          <p className='text-xs text-muted-foreground'>
-            {product.restaurant.name}
-          </p>
-        </div>
-        <h2 className='mt-1 text-xl font-semibold'>{product.name}</h2>
-        <div className='flex items-center justify-between mt-3'>
-          <h3 className='text-xl font-semibold'>
-            {formatCurrency(product.price)}
-          </h3>
-          <div className='flex items-center gap-3 text-center'>
-            <Button
-              variant='outline'
-              className='h-8 w-8 rounded-xl'
-              onClick={handleDecrease}
-            >
-              <ChevronLeftIcon />
-            </Button>
-            <p className='w-4'>{quantity}</p>
-            <Button
-              variant='destructive'
-              className='h-8 w-8 rounded-xl'
-              onClick={handleIncrease}
-            >
-              <ChevronRightIcon />
-            </Button>
-          </div>
-        </div>
+  const handleAddToCart = () => {
+    toggleCart()
+  }
 
-        <ScrollArea className='h-full'>
-          <div className='mt-6 space-y-3'>
-            <h4 className='font-semibold'>Sobre</h4>
-            <p className='text-sm text-muted-foreground'>
-              {product.description}
+  return (
+    <>
+      <div className='flex flex-col relative z-50 mt-[-1.5rem] rounded-t-3xl p-5 flex-auto overflow-hidden'>
+        <div className='flex-auto overflow-hidden'>
+          <div className='flex items-center gap-1.5'>
+            <Image
+              src={product.restaurant.avatarImageUrl}
+              alt={product.restaurant.name}
+              width={16}
+              height={16}
+              className='rounded-full'
+            />
+            <p className='text-xs text-muted-foreground'>
+              {product.restaurant.name}
             </p>
           </div>
-          <div className='mt-6 space-y-3'>
-            <div className='flex items-center gap-1.5'>
-              <ChefHatIcon size={18} />
-              <h4 className='font-semibold'>Ingredientes</h4>
+          <h2 className='mt-1 text-xl font-semibold'>{product.name}</h2>
+          <div className='flex items-center justify-between mt-3'>
+            <h3 className='text-xl font-semibold'>
+              {formatCurrency(product.price)}
+            </h3>
+            <div className='flex items-center gap-3 text-center'>
+              <Button
+                variant='outline'
+                className='h-8 w-8 rounded-xl'
+                onClick={handleDecrease}
+              >
+                <ChevronLeftIcon />
+              </Button>
+              <p className='w-4'>{quantity}</p>
+              <Button
+                variant='destructive'
+                className='h-8 w-8 rounded-xl'
+                onClick={handleIncrease}
+              >
+                <ChevronRightIcon />
+              </Button>
             </div>
-            <ul className='list-disc px-5 text-sm text-muted-foreground'>
-              {product.ingredients.map((ingredient) => (
-                <li key={ingredient}>{ingredient}</li>
-              ))}
-            </ul>
           </div>
-        </ScrollArea>
+
+          <ScrollArea className='h-full'>
+            <div className='mt-6 space-y-3'>
+              <h4 className='font-semibold'>Sobre</h4>
+              <p className='text-sm text-muted-foreground'>
+                {product.description}
+              </p>
+            </div>
+            <div className='mt-6 space-y-3'>
+              <div className='flex items-center gap-1.5'>
+                <ChefHatIcon size={18} />
+                <h4 className='font-semibold'>Ingredientes</h4>
+              </div>
+              <ul className='list-disc px-5 text-sm text-muted-foreground'>
+                {product.ingredients.map((ingredient) => (
+                  <li key={ingredient}>{ingredient}</li>
+                ))}
+              </ul>
+            </div>
+          </ScrollArea>
+        </div>
+        <Button className='rounded-full w-full' onClick={handleAddToCart}>
+          Adicionar à sacola
+        </Button>
       </div>
-      <Button className='rounded-full w-full'>Adicionar à sacola </Button>
-    </div>
+      <CartSheet />
+    </>
   )
 }
 
--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,39 +1,39 @@
-import { db } from "@/lib/prisma";
-import { notFound } from "next/navigation";
-import RestaurantHeader from "./components/header";
-import RestaurantCategories from "../components/categories";
+import { db } from '@/lib/prisma'
+import { notFound } from 'next/navigation'
+import RestaurantHeader from './components/header'
+import RestaurantCategories from './components/categories'
 
 interface RestaurantMenuPageParms {
-  params: Promise<{ slug: string }>;
-  searchParams: Promise<{ consumptionMethod: string }>;
+  params: Promise<{ slug: string }>
+  searchParams: Promise<{ consumptionMethod: string }>
 }
 
 const isConsumptionMethodValid = (consumptionMethod: string) => {
-  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
-};
+  return ['DINE_IN', 'TAKEAWAY'].includes(consumptionMethod.toUpperCase())
+}
 
 const RestaurantMenuPage = async ({
   params,
   searchParams,
 }: RestaurantMenuPageParms) => {
-  const { slug } = await params;
-  const { consumptionMethod } = await searchParams;
+  const { slug } = await params
+  const { consumptionMethod } = await searchParams
   if (!isConsumptionMethodValid(consumptionMethod)) {
-    return notFound();
+    return notFound()
   }
   const restaurant = await db.restaurant.findUnique({
     where: { slug },
     include: { menuCategories: { include: { products: true } } },
-  });
+  })
   if (!restaurant) {
-    return notFound();
+    return notFound()
   }
   return (
     <div>
       <RestaurantHeader restaurant={restaurant} />
       <RestaurantCategories restaurant={restaurant} />
     </div>
-  );
-};
+  )
+}
 
-export default RestaurantMenuPage;
+export default RestaurantMenuPage
