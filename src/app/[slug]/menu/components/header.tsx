be8a56118b441e284bb5ff11cc488cce92ae704e
marvieiradev <marcosvieira482@gmail.com>
2025-02-22 13:36:26 -0300

    feat: ensure all orders and back buttons work as expected

--- src/app/[slug]/menu/components/header.tsx
+++ src/app/[slug]/menu/components/header.tsx
@@ -1,24 +1,26 @@
-"use client";
-import { Restaurant } from "@prisma/client";
-import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+'use client'
+import { Restaurant } from '@prisma/client'
+import { ChevronLeftIcon, ScrollTextIcon } from 'lucide-react'
+import Image from 'next/image'
+import { useParams, useRouter } from 'next/navigation'
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 interface RestaurantHeaderProps {
-  restaurant: Pick<Restaurant, "name" | "coverImageUrl">;
+  restaurant: Pick<Restaurant, 'name' | 'coverImageUrl'>
 }
 
 const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
-  const router = useRouter();
-  const handleBackClick = () => router.back();
+  const { slug } = useParams<{ slug: string }>()
+  const router = useRouter()
+  const handleBackClick = () => router.back()
+  const handleOrderClick = () => router.push(`/${slug}/orders`)
   return (
-    <div className="relative h-[250px] w-full">
+    <div className='relative h-[250px] w-full'>
       <Button
-        variant="secondary"
-        size="icon"
-        className="absolute left-4 top-4 z-50 rounded-full"
+        variant='secondary'
+        size='icon'
+        className='absolute left-4 top-4 z-50 rounded-full'
         onClick={handleBackClick}
       >
         <ChevronLeftIcon />
@@ -27,17 +29,18 @@ const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
         src={restaurant.coverImageUrl}
         alt={restaurant.name}
         fill
-        className="object-cover"
+        className='object-cover'
       />
       <Button
-        variant="secondary"
-        size="icon"
-        className="absolute right-4 top-4 z-50 rounded-full"
+        variant='secondary'
+        size='icon'
+        className='absolute right-4 top-4 z-50 rounded-full'
+        onClick={handleOrderClick}
       >
         <ScrollTextIcon />
       </Button>
     </div>
-  );
-};
+  )
+}
 
-export default RestaurantHeader;
+export default RestaurantHeader
