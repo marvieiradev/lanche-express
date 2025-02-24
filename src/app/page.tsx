47e0c1357cf95502d195d620363d882f384da4a5
marvieiradev <marcosvieira482@gmail.com>
2025-02-24 18:54:07 -0300

    fix: remove test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   const router = useRouter()
   const handleStart = () => router.push(`/lanche-express`)
-  console.log(process.env.TEST)
   return (
     <div className='w-full h-full flex justify-center items-center'>
       <Button onClick={handleStart} className='rounded-full'>
